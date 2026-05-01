241c018f27a596d070e376de7118c202c27b5da8
William <william.sentosatio@hotmail.com>
2026-05-02 04:28:31 +0700

    renaming

--- src/features/landing-page/VideoPanel.tsx
+++ src/features/landing-page/VideoPanel.tsx
@@ -2,9 +2,12 @@ export const VideoPanel: React.FC = () => {
     return (
         <article className="w-full h-full shrink-0 flex flex-col justify-center items-center relative overflow-hidden">
             <div className="w-full h-full absolute left-0 top-0 bg-black/35 flex flex-col items-center justify-center text-white">
-                <h1 className="text-xl sm:text-9xl font-bold">WEBSITE NAME</h1>
-                <h2 className="text-base italic sm:text-2xl mt-4 sm:mt-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <h1 className="text-xl sm:text-9xl font-bold">
+                    William Sentosatio
+                </h1>
+                <h2 className="text-sm italic sm:text-3xl mt-4 sm:mt-8 font-smooch font-medium text-center">
+                    Collections of visual design for web components, UI
+                    libraries, and prototypes.
                 </h2>
             </div>
             <video
