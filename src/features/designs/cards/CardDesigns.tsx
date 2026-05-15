48e86ade444b4fc3f52d3c9ed18ca12a62423ed9
William <william.sentosatio@hotmail.com>
2026-05-15 21:03:31 +0700

    profile card

--- src/features/designs/cards/CardDesigns.tsx
+++ src/features/designs/cards/CardDesigns.tsx
@@ -1,4 +1,5 @@
 import { LivestreamVideoCard } from "@/src/libraries/components/cards/LivestreamVideoCard";
+import { ProfileCard } from "@/src/libraries/components/cards/ProfileCard";
 import { ThumbnaillessCard } from "@/src/libraries/components/cards/ThumbnaillessCard";
 import { VideoCard } from "@/src/libraries/components/cards/VideoCard";
 
@@ -9,23 +10,39 @@ export const CardDesigns: React.FC = () => {
                 Card Designs
             </h1>
             <div className="flex flex-wrap gap-4 px-16">
-                <div className="w-80 flex flex-col">
-                    <span className="my-auto text-xl">Video Card</span>
-                    <span className="opacity-75 italic text-sm mt-1 mb-6 font-barlow">
-                        Card, Video, Duration, VOD
-                    </span>
-                    <VideoCard
-                        username="captain_domo"
-                        avatar="captain_domo-avatar.png"
-                        duration={7847}
-                        thumbnail="captain_domo-content.jpg"
-                        title="USA vs France randomizer relay race"
-                    />
+                <div className="flex flex-col justify-between">
+                    <div className="w-80 flex flex-col justify-start">
+                        <span className="text-xl">Video Card</span>
+                        <span className="opacity-75 italic text-sm mt-1 mb-6 font-barlow">
+                            Card, Video, Duration, VOD
+                        </span>
+                        <VideoCard
+                            username="captain_domo"
+                            avatar="captain_domo-avatar.png"
+                            duration={7847}
+                            thumbnail="captain_domo-content.jpg"
+                            title="USA vs France randomizer relay race"
+                        />
+                    </div>
+                    <div className="w-80 flex flex-col justify-start">
+                        <span className="text-xl">Thumbnail-less Card</span>
+                        <span className="opacity-75 italic text-sm mt-1 mb-6 font-barlow">
+                            Card, Post, Image, Video, Count, No-Thumbnail
+                        </span>
+                        <ThumbnaillessCard
+                            username="midetaka_hiyazaki"
+                            content={{
+                                stars: 4594859,
+                                type: ".zip",
+                                files: 21,
+                                size: 73947291,
+                            }}
+                            title="The Iron Mass vol.1"
+                        />
+                    </div>
                 </div>
-                <div className="w-80 flex flex-col">
-                    <span className="my-auto text-xl">
-                        Livestream Video Card
-                    </span>
+                <div className="w-80 flex flex-col justify-start">
+                    <span className="text-xl">Livestream Video Card</span>
                     <span className="opacity-75 italic text-sm mt-1 mb-6 font-barlow">
                         Card, Video, Livestream, Viewers
                     </span>
@@ -38,20 +55,23 @@ export const CardDesigns: React.FC = () => {
                         isLive
                     />
                 </div>
-                <div className="w-80 flex flex-col">
-                    <span className="my-auto text-xl">Thumbnail-less Card</span>
+                <div className="w-80 flex flex-col justify-start">
+                    <span className="text-xl">Profile Card</span>
                     <span className="opacity-75 italic text-sm mt-1 mb-6 font-barlow">
-                        Card, Post, Image, Video, Count, No-Thumbnail
+                        Card, Profile, Description, Portrait, Tags
                     </span>
-                    <ThumbnaillessCard
-                        username="midetaka_hiyazaki"
-                        content={{
-                            stars: 4594859,
-                            type: ".zip",
-                            files: 21,
-                            size: 73947291,
-                        }}
-                        title="The Iron Mass vol.1"
+                    <ProfileCard
+                        banner="pexels-pspov-3046628-medium.jpg"
+                        community="Brookstone Haven Scuba Club"
+                        description="Vestibulum commodo nec nisi vel laoreet."
+                        members={37}
+                        tags={[
+                            "Diving",
+                            "Friendly",
+                            "Sea",
+                            "Sports",
+                            "Coaching",
+                        ]}
                     />
                 </div>
             </div>
